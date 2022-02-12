03fd3334cdeafc2952debfd6b7dd488362f5c3aa
AFOP <4592@holbertonschool.com>
2022-02-11 17:46:06 -0800

    Ultima de las ultimas

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * positive or negative
- * comments in C source code.
- * Please use it consistently.
- *
- * Description:  A column of asterisks on the left side,
- * with beginning and ending almost-blank lines.
- */
-/* Positivo o Negativo */
+   * main - positive or negative
+   * Comments in C source code.
+   * Please use it consistently.
+   *
+   * Description: A column of asterisks on the left side,
+   * With beginning and ending almost-blank lines.
+   *
+   */
 int main(void)
 {
 	int n;
