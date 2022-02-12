08282db22579a0b2e9eb1d3367110d7001c70e50
AFOP <4592@holbertonschool.com>
2022-02-11 17:41:04 -0800

    Ultima de las ultimas

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,14 @@
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
-/*
+/**
  * positive or negative
  * comments in C source code.
  * Please use it consistently.
  *
  * Description:  A column of asterisks on the left side,
  * with beginning and ending almost-blank lines.
- * function main: return zero
+ * 
  */
 int main(void)
 {
