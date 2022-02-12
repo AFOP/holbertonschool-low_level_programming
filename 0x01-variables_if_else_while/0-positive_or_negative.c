223d81c1026925646e8521f591b07e4161e50e2d
AFOP <4592@holbertonschool.com>
2022-02-11 16:16:11 -0800

    Intento 999 para el primer punto

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * more headers goes there 
- * print_hello - Prints hello 
- * betty style doc for function main goes there
- */
+* more headers goes there 
+* print_hello - Prints hello 
+* betty style doc for function main goes there
+*/
 int main(void)
 {
-        int n;
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-        }
+	}
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
-        }
+	}
 	else
 	{
 		 printf("%d is negative\n", n);
-        }
+	}
 	return (0);
 }
 
