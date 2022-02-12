197ce7c541e8a96f4ad7d58cf749edeb163523c5
AFOP <4592@holbertonschool.com>
2022-02-11 19:26:14 -0800

    actualizacion

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() {
+    int intType;
+    float floatType;
+    double doubleType;
+    char charType;
+    long int lgintType;
+    
+
+    // sizeof evaluates the size of a variable
+    printf("Size of char: %zu bytes\n", sizeof(CharType));
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of long int: %zu bytes\n", sizeof(lgintType);
+    printf("Size of float: %zu byte\n", sizeof(floatType));
+    
+    return (0);
+}
+
