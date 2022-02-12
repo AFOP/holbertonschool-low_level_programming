510fe41b6d0f33d7f8fab056d8740ecc1ba2ac0e
AFOP <4592@holbertonschool.com>
2022-02-11 20:30:20 -0800

    Numero 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0;n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
+
