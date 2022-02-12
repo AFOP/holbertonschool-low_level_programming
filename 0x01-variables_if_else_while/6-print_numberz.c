ff63d7f6d29256b89bfc07145c14d342ec8a7357
AFOP <4592@holbertonschool.com>
2022-02-11 20:38:40 -0800

    Sexta

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	putchar(n);
+	putchar('\n');
+	return (0);
+}
+
