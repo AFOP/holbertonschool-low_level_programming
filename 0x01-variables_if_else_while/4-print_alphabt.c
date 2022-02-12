6471f7fb69f7532a79a68bda72ae8e0f795d6e33
AFOP <4592@holbertonschool.com>
2022-02-11 20:19:11 -0800

    La cuarta

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+	
+	for (az = 'a'; az <= 'z'; az++)
+	{
+		if (az != 'e' && az != 'q')
+		{
+			putchar(az);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
