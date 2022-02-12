5a7ce97b21ce3feb0f7b3e9088f59c0d0e9b132d
AFOP <4592@holbertonschool.com>
2022-02-11 20:45:59 -0800

    la 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	char za;
+
+	for (za = 'z'; za >= 'a'; za--)
+	{
+		putchar(za);
+	}
+	putchar('\n');
+	return (0);
+}
+
