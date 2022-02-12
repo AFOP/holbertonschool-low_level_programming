767158994685bac6b407c2557df2a31a760348dc
AFOP <4592@holbertonschool.com>
2022-02-11 19:56:38 -0800

    La segunda

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	char az;
+	
+	for (az = 'a'; az <= 'z'; az++)
+	{	
+		putchar(az);
+	}
+	putchar('\n');
+	return (0);
+}
+
