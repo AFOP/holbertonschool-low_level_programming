f4a960a124893cb8c0c17d18f7112848988c4736
AFOP <4592@holbertonschool.com>
2022-02-11 20:56:50 -0800

    la nueve

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the function find the positive, negativo o zero of n
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
