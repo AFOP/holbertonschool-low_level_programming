a4b88f54cc29ebfdfe9bc1b37028464b113122ba
AFOP <4592@holbertonschool.com>
2022-02-16 13:56:48 -0800

    Update point one

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+* postive_or_negative - function prints positive or negative 
+* Return: 0
+*/ 
 void positive_or_negative(int i)
 {
 /* your code goes there */
