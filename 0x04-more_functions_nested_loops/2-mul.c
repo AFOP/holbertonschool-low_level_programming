7b2df48d7234efab3b17983bc532350026a466a4
AFOP <4592@holbertonschool.com>
2022-02-17 19:53:44 -0800

    Update point two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplication two integers.
+ * @a: variable integer
+ * @b: variable integer
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
