e31fc55c8b8f8c8904b16784f010969a13915a79
AFOP <4592@holbertonschool.com>
2022-02-18 19:25:34 -0800

    Firts example

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n < 100; n++)
+	{
+		printf("%d", n);
+	}	
+	return (0);
+}
