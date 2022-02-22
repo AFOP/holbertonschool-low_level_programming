cbc089a80ae2eebafc611c7049f91ac3fba045a3
AFOP <4592@holbertonschool.com>
2022-02-21 20:15:24 -0800

    Update point 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,21 +3,19 @@
 /**
  * print_rev - prints a string, in reverse, followed by a new line.
  * @s: pointer of the string
- * @c: variable for save the string s
  * Return: Nothing
  */
 void print_rev(char *s)
 {
 	int i = 0;
-	char *c = s;
 
-	while (c[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
 	while (i >= 0)
 	{
-		_putchar(c[--i]);
+		_putchar(s[--i]);
 	}
 	_putchar('\n');
 }
