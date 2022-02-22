5ea7c779138e3b611c0966732db55736c282ff81
AFOP <4592@holbertonschool.com>
2022-02-21 16:05:18 -0800

    Update point 1 - two variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: is a variable type integer with value 98
+ * @b: is a variable type integer with value 42
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
