7af953530c8483fe333d811cfa624bd0a9e283b8
AFOP <4592@holbertonschool.com>
2022-02-22 17:42:03 -0800

    Update point 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string, followed by a new line.
+ * @str: pointer of the string
+ * Return: Nothing
+ */
+void puts_half(char *str)
+{
+	int i, c;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (c = i / 2; c < i; c++)
+	{
+		_putchar(str[c]);
+	}
+	_putchar('\n');
+}
