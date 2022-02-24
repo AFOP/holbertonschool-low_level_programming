0c45075fdc6227bcd8bc57e4809a5b2810c65937
AFOP <4592@holbertonschool.com>
2022-02-24 14:04:12 -0800

    Update point one

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,13 +1,33 @@
 #include "main.h"
-
 /**
- * _strncat - concatenates two strings with variable n.
+ * _strncat - concatenates two strings.
  * @src: pointer of the string one
  * @dest: pointer of the string two
- * @n: variable integer
+ * @n: number of characteres for show
  * Return: a pointer with the resulting of the string
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int srclen = 0, i = 0;
+	char *temp = dest, *start = src;
+
+	while (*src)
+	{
+		srclen++;
+		src++;
+	}
+
+	while (*dest)
+		dest++;
+
+	if (n > srclen)
+		n = srclen;
+
+	src = start;
+
+	for (; i < n; i++)
+		*dest++ = *src++;
+
+	*dest = '\0';
+	return (temp);
 }
