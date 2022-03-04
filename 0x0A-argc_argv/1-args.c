b5d0a3f171f6c3853016a481a1bf4e5ca577d061
AFOP <4592@holbertonschool.com>
2022-03-04 15:38:28 -0800

    Update point one

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - prints its name, followed by a new line
+ * main - prints the number of arguments passed into it
  * @argc: The size of the argv array
  * @argv: An array of size argc
  * Return: zero
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
