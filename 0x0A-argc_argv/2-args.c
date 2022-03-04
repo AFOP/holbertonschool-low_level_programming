32fe40ea2169531771aec053473adda59ca18fe0
AFOP <4592@holbertonschool.com>
2022-03-04 15:44:47 -0800

    Update point two

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives
+ * @argc: The size of the argv array
+ * @argv: An array of size argc
+ * Return: zero
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
