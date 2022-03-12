b478b368c38f205e87c950ac15126f5f6dae63c4
AFOP <4592@holbertonschool.com>
2022-03-11 18:56:59 -0800

    Update point one

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,16 +15,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		S2 = "";
 
 	if (n >= strlen(s2))
 		n = strlen(s2);
 
 	concat = malloc(sizeof(char) * (strlen(s1) + strlen(s2)));
 	if (concat == NULL)
+		free(concat);
 		return (NULL);
-
 
 	for (i = 0; i < strlen(s1); i++)
 	{
