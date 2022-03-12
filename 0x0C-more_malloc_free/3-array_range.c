a91e84c7909ab8b69acec46e8b1c0f6bbcd00c52
AFOP <4592@holbertonschool.com>
2022-03-11 19:44:33 -0800

    Update point Three

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,22 +11,21 @@
  */
 int *array_range(int min, int max)
 {
-	int i, *arr;
+	int i, len, *arr;
 
 	if (min > max)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * (max - min + 1));
+	len = max - min + 1;
 
+	arr = malloc(sizeof(int) * len);
 	if (arr == NULL)
-	{
-		free(arr);
 		return (NULL);
-	}
 
-	for (i = 0; i < max; i++)
+	for (i = 0; i < len; i++)
 	{
 		arr[i] = min + i;
 	}
+
 	return (arr);
 }
