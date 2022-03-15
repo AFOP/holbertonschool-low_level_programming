771d6d1e0fcc0b428987588d7533adfc4a351e48
AFOP <4592@holbertonschool.com>
2022-03-14 20:00:33 -0700

    Update point three

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,11 @@
 #define _dog_
 
 /**
+ * dog_t - new struct of the dog
+ */
+typedef struct dog dog_t;
+
+/**
  * struct dog - struct of the dog
  * @name: pointer to a char
  * @age: age of the dog
@@ -13,6 +18,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
