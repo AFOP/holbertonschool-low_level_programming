9be65474275315fcb4c35531d3ca91e809be0aba
AFOP <4592@holbertonschool.com>
2022-03-14 18:08:40 -0700

    Update point zero

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef _dog_
 #define _dog_
 
+/**
+ * struct dog - struct of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: struct called "dog"
+ */
 struct dog 
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 #endif
