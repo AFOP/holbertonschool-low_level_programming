82c698ea35906c7a09135fc14feb330678b1f6a8
AFOP <4592@holbertonschool.com>
2022-03-17 08:36:33 -0700

    Update point one

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	size_t i = 0;
 
 	if (array != NULL && size != 0)
+	{
 		while (i < size)
 		{
 			action(array[i]);
 			i++;
 		}
+	}
 }
