1a031a9d037503910f58fb707907da4955440293
AFOP <4592@holbertonschool.com>
2022-03-18 20:47:46 -0700

    Update point one

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
 	if (separator == NULL)
 		separator = "";
 
