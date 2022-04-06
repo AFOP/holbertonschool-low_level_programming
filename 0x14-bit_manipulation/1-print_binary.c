38a7e8dec7c0ea7e17de83bc3880801d506e060b
AFOP <4592@holbertonschool.com>
2022-04-05 17:01:22 -0700

    Update task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number
+ * @n: number
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	if (n >> 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
