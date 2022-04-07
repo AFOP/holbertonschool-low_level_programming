9dd2dcbc53574640f551322cb13149db56204e04
AFOP <4592@holbertonschool.com>
2022-04-06 20:07:08 -0700

    Update task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - function that creates a file.
  * @filename: is the name of the file to create
  * @text_content:  pointer of the text_content
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
