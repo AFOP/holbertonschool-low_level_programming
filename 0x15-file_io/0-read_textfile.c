06c4af8e4256051044182b330d76d2a42a5343e9
AFOP <4592@holbertonschool.com>
2022-04-06 18:53:17 -0700

    Update task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,17 +3,17 @@
  * read_textfile - function that reads a text file and prints it
  * @filename: pointer of the file name
  * @letters:  is the number of letters it should read and print
- * Return: the actual number of letters it could read and print and if fail return 0
+ * Return: the actual number of letters if fail return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, r, w;
 	char *buf;
-	
+
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 || fd == NULL)
+	if (fd == -1)
 	{
 		close(fd);
 		return (0);
@@ -23,11 +23,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	r = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, letters);
-	if (r == -1 || r == NULL || w == -1 || w == NULL)
+	if (r == -1 || w == -1)
 		return (0);
 	close(fd);
 	free(buf);
 	return (r);
 }
-	
-
