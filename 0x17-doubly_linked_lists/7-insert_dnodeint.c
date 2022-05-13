2fe683f39b3ad8eb7d77c78742152900f478a02d
AFOP <4592@holbertonschool.com>
2022-05-12 23:08:55 -0500

    Update Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,6 +13,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (h == NULL)
 		return (NULL);
+	if (*h == NULL)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
