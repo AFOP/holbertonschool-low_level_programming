ad7718ce8007a1dda51af24ffd538089fd19c381
AFOP <4592@holbertonschool.com>
2022-05-12 20:02:17 -0500

    Update Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,8 +10,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *compare;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (head == NULL )
 		return (NULL);
+	if (index <= 0)
+		return (NULL);	
 	compare = head;
 	while (compare->next != NULL)
 	{
---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
+ * @head: double pointer to head
+ * Return: if the list is empty, return 0
+ */
+int sum_dlistint(dlistint_t *head)
+{
+
+}
