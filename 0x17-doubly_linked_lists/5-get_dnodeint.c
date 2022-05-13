0f5c22157609a3c8204bd904d87aac7abd969d6d
AFOP <4592@holbertonschool.com>
2022-05-12 19:34:17 -0500

    Update Task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,7 +10,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *compare;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (index <= 0)
 		return (NULL);
 	compare = head;
 	while (compare->next != NULL)
