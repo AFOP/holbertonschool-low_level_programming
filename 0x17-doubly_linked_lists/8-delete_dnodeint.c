6d12f92a0140ac9a57c2422d315bc68439aa6d56
AFOP <4592@holbertonschool.com>
2022-05-13 23:44:26 -0500

    Update Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *del_n, *nodo;
 	unsigned int i = 0;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 	del_n = nodo = *head;
 	while (nodo != NULL)
