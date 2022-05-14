1e4d773861331e3f754a9d5d4868bb674465ef78
AFOP <4592@holbertonschool.com>
2022-05-13 22:05:44 -0500

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,60 @@
 #include "lists.h"
 /**
+ * del_node - delete the node
+ * @del_n: pointer of the node
+ * Return: Nothing
+ */
+void del_node(dlistint_t *del_n)
+{
+	del_n->next = NULL;
+	del_n->prev = NULL;
+	del_n = NULL;
+}
+/**
  * delete_dnodeint_at_index - deletes the node at index
  * @head: double pointer to head
  * @index: index of the position of the node
  * Return: 1 if it succeeded, -1 if it failed
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *del_n;
+	dlistint_t *del_n, *nodo;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-	if (idx == 0)
+	del_n = nodo = *head;
+	if (head == NULL)
+		return (-1);
+	while (nodo != NULL)
 	{
-		new = add_dnodeint(h, n);
-		return (new);
-	}
-	inser_n = *h;
-	i = 1;
-    if (head != NULL)
-            while (head->prev != NULL)
-                head = head->prev;
-	while (inser_n != NULL)
-	{
-		if (i == idx)
+		if (index == 0)
 		{
-			if (inser_n->next == NULL)
-				new = add_dnodeint_end(h, n);
-			else
+			nodo = nodo->next;
+			*head = nodo;
+			del_node(del_n);
+			return (1);
+		}
+		else
+		{
+			if (i == index && nodo->next == NULL)
 			{
-				new = malloc(sizeof(dlistint_t));
-				if (new != NULL)
-				{
-					new->n = n;
-					new->next = inser_n->next;
-					new->prev = inser_n;
-					inser_n->next->prev = new;
-					inser_n->next = new;
-					return (new);
-				}
+				del_n = nodo;
+				nodo = nodo->prev;
+				nodo->next = NULL;
+				del_node(del_n);
+				return (1);
+			}
+			else if (i == index)
+			{
+				del_n = nodo;
+				nodo = nodo->prev;
+				nodo->next = del_n->next;
+				nodo->next->prev = nodo;
+				del_node(del_n);
+				return (1);
 			}
 		}
-		inser_n = inser_n->next;
+		nodo = nodo->next;
 		i++;
 	}
-	return (NULL);
-
+	return (-1);
 }
