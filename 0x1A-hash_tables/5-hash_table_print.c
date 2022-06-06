74015ea846356560dee00c7923d4e90f876425b1
AFOP <4592@holbertonschool.com>
2022-06-05 22:51:47 -0500

    Update task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,25 +9,25 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i, state = 0;
 
-    if (ht == NULL)
-        return;
-    printf("{");
-    for (i = 0; i < ht->size; i++)
-    {
-        if (ht->array[i] == NULL)
-            continue;
-        if (ht->array[0] == NULL && state == 0)
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] == NULL)
+			continue;
+		if (ht->array[0] == NULL && state == 0)
 		{
-            printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			state = 1;
 		}
 		else if (ht->array[0] == NULL)
 			printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-        while (ht->array[i]->next != NULL)
-        {
-           	ht->array[i] = ht->array[i]->next;
+		while (ht->array[i]->next != NULL)
+		{
+			ht->array[i] = ht->array[i]->next;
 			printf(", '%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-        }
-    }
-    printf("}\n");
+		}
+	}
+	printf("}\n");
 }
