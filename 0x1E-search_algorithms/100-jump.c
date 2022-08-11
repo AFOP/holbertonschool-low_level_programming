c269cdaeac4e63897221c5c6b0a217596a55a31d
AFOP <4592@holbertonschool.com>
2022-08-11 16:55:57 -0500

    Update task 100

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,10 +7,11 @@
  * @b: value two
  * Return: return min value
  */
-int min(int a, int b){
-    if(b>a)
-        return a;
-    return b;
+int min(int a, int b)
+{
+	if (b > a)
+		return (a);
+	return (b);
 }
 /**
  * iqual - function that return idex of iqual
@@ -18,41 +19,42 @@ int min(int a, int b){
  * @prev: is the first range of array
  * @step: is the last range of array
  * @value: is the value to search
+ * @hight: is the last index of array
  * Return: return index of array
  */
 int iqual(int *array, int prev, int step, int value, int hight)
 {
-    int r = sqrt(hight);
-    int indx = (prev - r);
-    int prev2 = (prev - r);
+	int r = sqrt(hight);
+	int indx = (prev - r);
+	int prev2 = (prev - r);
 
-    if (prev2 == prev)
-    {
-        prev2 = 0;
-        indx = 0;
-    }
-    if (prev > hight)
-    {
-        indx = prev - r;
-        prev2 = indx;
-    }
-    if (value == 0)
-    {
-        prev2 = 0;
-        prev = step;
-        indx = prev2;
-    }
-    printf("Value found between indexes [%d] and [%d]\n", prev2, prev);
-    while (indx < hight + 1)
-    {
-        printf("Value checked array[%d] = [%d]\n", indx, array[indx]);
-        if (array[indx] == value)
-        {
-            return (indx);
-        }
-        indx++;
-    }
-    return (-1);
+	if (prev2 == prev)
+	{
+		prev2 = 0;
+		indx = 0;
+	}
+	if (prev > hight)
+	{
+		indx = prev - r;
+		prev2 = indx;
+	}
+	if (value == 0)
+	{
+		prev2 = 0;
+		prev = step;
+		indx = prev2;
+	}
+	printf("Value found between indexes [%d] and [%d]\n", prev2, prev);
+	while (indx < hight + 1)
+	{
+		printf("Value checked array[%d] = [%d]\n", indx, array[indx]);
+		if (array[indx] == value)
+		{
+			return (indx);
+		}
+		indx++;
+	}
+	return (-1);
 }
 /**
  * jump_search - function that searches for a value in a sorted array
@@ -64,23 +66,23 @@ int iqual(int *array, int prev, int step, int value, int hight)
  */
 int jump_search(int *array, size_t size, int value)
 {
-    int index = 0, prev = 0, hight = size - 1;
-    int step = sqrt(hight);
+	int index = 0, prev = 0, hight = size - 1;
+	int step = sqrt(hight);
 
-    if (size <= 0)
-        return (-1);
-    if (array)
-    {
-        while (array[min(prev, hight)] < value)
-        {
-            printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
-            prev = step;
-            step += sqrt(size);
-            if (prev > hight)
-                break;
-        }   
-        index = iqual(array, prev, step, value, hight);
-        return (index);
-    }
-    return (-1);
+	if (size <= 0)
+		return (-1);
+	if (array)
+	{
+		while (array[min(prev, hight)] < value)
+		{
+			printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+			prev = step;
+			step += sqrt(size);
+			if (prev > hight)
+				break;
+		}
+		index = iqual(array, prev, step, value, hight);
+		return (index);
+	}
+	return (-1);
 }
